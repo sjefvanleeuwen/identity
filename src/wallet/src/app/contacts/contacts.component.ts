a998eb9de55ca7d56ea951f02aa7c290eb8e4002
Marten <you@example.com>
2019-08-28 03:38:48 -0700

    add chatbot

--- src/wallet/src/app/contacts/contacts.component.ts
+++ src/wallet/src/app/contacts/contacts.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+declare global {
+  interface Window { chatWindow: any; }
+}
+
+window.chatWindow = window.chatWindow || {};
+declare const Bubbles: any;
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
@@ -10,6 +17,47 @@ export class ContactsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    window.chatWindow = new Bubbles(
+      document.getElementById('chat'), // ...passing HTML container element...
+      "window.chatWindow" // ...and name of the function as a parameter
+    );
+
+    window.chatWindow.talk(
+      // pass your JSON/JavaScript object to `.talk()` function where
+      // you define how the conversation between the bot and user will go
+      {
+        // "ice" (as in "breaking the ice") is a required conversation object
+        // that maps the first thing the bot will say to the user
+        "ice": {
+    
+          // "says" defines an array of sequential bubbles
+          // that the bot will produce
+          "says": [ "Hey!", "Can I have a banana?" ],
+    
+          // "reply" is an array of possible options the user can pick from
+          // as a reply
+          "reply" : [
+            {
+              "question" : "🍌",  // label for the reply option
+              "answer" : "banana",  // key for the next conversation object
+            }
+          ]
+        }, // end required "ice" conversation object
+    
+        // another conversation object that can be queued from within
+        // any other conversation object, including itself
+        "banana" : {
+          "says" : [ "Thank you!", "Can I have another banana?"],
+          "reply": [
+            {
+              "question": "🍌🍌",
+              "answer": "banana"
+            }
+          ]
+        } // end conversation object
+      } // end conversation object
+    );
   }
     messages = [
       {
