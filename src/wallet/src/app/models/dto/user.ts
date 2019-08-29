848623e65f2714da1b94f5aeac4494b16437dac1
sjefvanleeuwen <@sjefvanleeuwen>
2019-08-29 22:35:34 +0200

    adding user dto object

---
+++ src/wallet/src/app/models/dto/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    public bsn: number;
+    public loggedIn: boolean;
+    public isInwoner: boolean;
+    public isProfessional: boolean;
+    public token: string;
+    public userName: string;
+    public password: string;
+}
